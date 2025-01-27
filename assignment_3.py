93948e4730d696cd78e69cea52dc512e59a10520
kayyy01 <136058945+kayyy01@users.noreply.github.com>
2025-01-27 03:52:31 +0000

    Second commit

--- assignment_3.py
+++ assignment_3.py
@@ -1,19 +1,19 @@
 sentence = input("enter a sentence  ")
 
 #Output Sentence Details
-print(len(sentence))
-print(len(sentence.split(" ")))
-print(sentence.split(" ")[0])
-print(sentence.split(" ")[-1])
+print(len(sentence)) #shows the total number of characters including spaces
+print(len(sentence.split(" "))) #shows the total number of words
+print(sentence.split(" ")[0]) #shows the first word
+print(sentence.split(" ")[-1]) #shows the last word
 
 #Modify the sentence
-print(sentence.upper())
-print(sentence.lower())
-print(sentence.replace(" ","-"))
+print(sentence.upper()) #converts input to uppercase
+print(sentence.lower()) #converts input to lowercase
+print(sentence.replace(" ","-")) #replaces spaces with hyphens
 
 
 #Indexing & Slicing
-print(sentence[0:3])
-print(sentence[-3:])
-print(sentence[::-1])
+print(sentence[0:3]) #shows the first 3 characters
+print(sentence[-3:]) #shows the last 3 characters
+print(sentence[::-1]) #reverses the sentence
 

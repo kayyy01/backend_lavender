0ff2eae246ef1c7e8ef4a41257d9aec389d7182d
kayyy01 <kadjeiyeboah96@gmail.com>
2025-02-18 16:20:58 +0000

    second commit

--- STS_10.py
+++ STS_10.py
@@ -1,38 +1,67 @@
-user_name = input("what is your name ")
-age = input("what's your age  ")
-fav_color = input("what's your favourite colour  ")
+# user_name = input("what is your name ")
+# age = input("what's your age  ")
+# fav_color = input("what's your favourite colour  ")
 
 
-friends_name = input("what are the names of your friends: ")
+# friends_name = input("what are the names of your friends: ")
 
-user = {
-    "name": user_name,
-    "age" : age,
-    "favourite color" : fav_color,    
-}
+# user = {
+#     "name": user_name,
+#     "age" : age,
+#     "favourite color" : fav_color,    
+# }
 
-friends_name_list = friends_name.split(",")
+# friends_name_list = friends_name.split(",")
 
-print(user)
-print("If you want to make a change 'Yes', else type 'No' ")
+# print(user)
+# print("If you want to make a change 'Yes', else type 'No' ")
 
-change = input("do you want to change? ").capitalize()
+# change = input("do you want to change? ").capitalize()
 
-if change == "Yes":
-    User_name = input("what is your name ")
-    Age = input("what's your age  ")
-    Fav_color = input("what's your favourite colour  ")
+# # if change == "Yes":
+# #     User_name = input("what is your name ")
+# #     Age = input("what's your age  ")
+# #     Fav_color = input("what's your favourite colour  ")
 
-    user["age"] = age
-    user["Favourite_color"] = Fav_color
-    print("updated user", user)
-    print("list of friends:", friends_name)
+# #     user["age"] = age
+# #     user["Favourite_color"] = Fav_color
+# #     print("updated user", user)
+# #     print("list of friends:", friends_name)
 
-else: 
-    print("list of friends", friends_name_list)
+# # else: 
+# #     print("list of friends", friends_name_list)
 
 
-# if condition:
-#run if-else if there is one condition
+# # if condition:
+# #run if-else if there is one condition
 
-#run if-elif-else if there is multiple conditions
+# #run if-elif-else if there is multiple conditions
+
+# if change == "Yes": 
+#     print("where do you want to change? ")
+#     print(
+#         "1. name",
+#         "2. age",
+#         "3. favourite color",
+        
+#     )
+#     loc_list = loc.split(",")
+
+#     #for loop
+#for loop helps us go through each item in a list one by one
+#     #loop: do something repeatedly
+
+#     for item in loc_list:
+#         print(item)
+
+names = ["kay", "mina", "jike", "blyss"]
+for i in range(len(names)): #range creates an intravel with the number of items in the list
+    print("hello world")
+
+#operators
+/ + - * // % **
+
+#continue/ break a for loop
+#when a for loop starts until a certain condition is met it will not stop
+#to stop a for loop use break 
+# "continue" skip a code block and continue the loop 
